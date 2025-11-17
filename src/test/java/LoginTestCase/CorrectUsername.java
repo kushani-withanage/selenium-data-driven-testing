3fabe23632464975fc5cda990e6095a9557017ff
Kushani21 <dhanushikakwk@gmail.com>
2025-11-17 10:01:30 +0530

    test: add 4 separate login validation classes and TestNG suite configuration
    - Created individual test classes for different OrangeHRM login checks
    - Added testng.xml to run all login test classes in one suite

--- src/test/java/LoginTestCase/CorrectUsername.java
+++ src/test/java/LoginTestCase/CorrectUsername.java
@@ -1,4 +1,39 @@
 package LoginTestCase;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
 public class CorrectUsername {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void openPage(){
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    }
+
+    @Test
+    public void bothCorrectTesting(){
+        WebElement userName = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input"));
+        userName.sendKeys("Admin");
+        WebElement password = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input"));
+        password.sendKeys("admin123000");
+        WebElement loginBtn = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button"));
+        loginBtn.click();
+
+    }
+
+    @AfterMethod
+    public void quitBrowser(){
+        driver.quit();
+    }
 }
