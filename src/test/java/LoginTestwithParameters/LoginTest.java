8a442f1da9393419dfe5271498c2a2f6d7a05e47
Kushani21 <dhanushikakwk@gmail.com>
2025-11-17 13:34:49 +0530

    refactor : merge login tests into one class and add XML File based data-driven parameterization

---
+++ src/test/java/LoginTestwithParameters/LoginTest.java
@@ -0,0 +1,50 @@
+package LoginTestwithParameters;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class LoginTest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void openPage(){
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    }
+
+    @Test
+    @Parameters({"username","password","verification"})
+    public void bothCorrectTesting(String uName,String passWord,String expectValidation){
+        WebElement userName = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input"));
+        userName.sendKeys(uName);
+        WebElement password = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input"));
+        password.sendKeys(passWord);
+        WebElement loginBtn = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button"));
+        loginBtn.click();
+
+        boolean urlVerification=driver.getCurrentUrl().contains("dashboard");
+
+        if(expectValidation.equals("valid")){
+            Assert.assertTrue(urlVerification,"Expecting login success but not navigate to dashboard");
+        }else{
+            Assert.assertTrue(urlVerification,"Expecting login fail but navigate to dashboard");
+        }
+
+    }
+
+    @AfterMethod
+    public void quitBrowser(){
+        driver.quit();
+    }
+}
